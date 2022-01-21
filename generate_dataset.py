40cab32611637f71b58c56ec3d8102f794f2984a
BeetrootWang <wzt12345678@gmail.com>
2022-01-21 15:59:49 -0500

    .

--- generate_dataset.py
+++ generate_dataset.py
@@ -2,13 +2,13 @@
 import numpy as np
 
 np.random.seed(0)
-training_size = 1e3
-testing_size = 1e3
+training_size = 1000
+testing_size = 1000
 
 for i in range(10):
-    training_dataset = np.random.uniform(-1,1,training_size)
-    testing_dataste = np.random.uniform(-1,1,training_size)
+    training_dataset = np.random.uniform(-1.,1.,training_size)
+    testing_dataste = np.random.uniform(-1.,1.,training_size)
     training_filename = 'data/training_' + str(training_size) + '_' + str(i)
-    testing_filename = 'data/training_' + str(training_size) + '_' + str(i)
+    testing_filename = 'data/testing_' + str(training_size) + '_' + str(i)
     np.save(training_filename, training_dataset)
     np.save(testing_filename, testing_dataste)
